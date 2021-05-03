1ca38cfc397c6ba322bb101017f050fea38ecade
施舒阳 <416871775@qq.com>
2021-05-03 11:12:16 +0800

    怎么这么多乱七八糟的文件

---
+++ 随机数生成器/随机数生成器/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace 随机数生成器
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            if (int.Parse(text1.Text) < int.Parse(text2.Text))
+            label1.Text = rd.Next(int.Parse(text1.Text), int.Parse(text2.Text)+1).ToString() ;
+
+        }
+    }
+}
